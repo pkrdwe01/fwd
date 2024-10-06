62c86e6632114d224a96c89997711a093adca44f
pkrdwe01 <116544327+pkrdwe01@users.noreply.github.com>
2024-10-06 13:15:46 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+tar -xvf ngrok.tgz > /dev/null 2>&1
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
